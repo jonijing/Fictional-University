5716b9c3cb7b4311f5e30d7017f5121ce1db7e79
jonijing <112223240+jonijing@users.noreply.github.com>
2022-12-04 13:24:02 +0800

    Famous loop for index, page, and single.php

--- page.php
+++ page.php
@@ -2,6 +2,7 @@
 
   while(have_posts()) {
     the_post(); ?>
+    This is a page
     <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
     <p><?php the_content(); ?></p>
     <hr>
